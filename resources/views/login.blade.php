d8851f1e5a567489c39becd56cb298e301ce3c4f
Anton <526473+gb96@users.noreply.github.com>
2022-04-18 05:10:36 +0700

    initial

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
 <body>
-	<h3>Halaman Login Sederhana</h3>
-	<form action="{{ route('login') }}" method="post" class="form-input">	
+	<div class="col-lg-6">
+		<h3>Halaman Login</h3>
+		<form action="{{ route('login') }}" method="post" class="form-input">	
+			@csrf
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Email</label>
+				<input type="text" class="form-control" id="exampleFormControlInput1x" name="username">
+			</div>
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Password</label>
+				<input type="password" class="form-control" id="exampleFormControlInput1" name="password">
+			</div>
+			<button
+				type="submit"
+				class="btn btn-success">Login</button>
+		</form>
+	</div>
+
+	<!-- <form action="{{ route('login') }}" method="post" class="form-input">	
     @csrf	
 		<table>
 			<tr>
@@ -21,6 +38,6 @@
 				<td><input type="submit" name="login" value="Log In"></td>
 			</tr>
 		</table>
-	</form>
+	</form> -->
 </body>
 </html>
